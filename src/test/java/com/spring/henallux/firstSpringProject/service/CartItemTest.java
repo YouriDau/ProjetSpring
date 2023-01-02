f2dbe5c1e18f36f6332e00dd3c61589240e26caa
Timothé Ciciotti <timciciotti@gmail.com>
2023-01-02 11:43:23 +0100

    a

---
+++ src/test/java/com/spring/henallux/firstSpringProject/service/CartItemTest.java
@@ -0,0 +1,17 @@
+package com.spring.henallux.firstSpringProject.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartItemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+}
